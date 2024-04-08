34d1b00e0b4ad181f9a113c48ebbf605756ccb48
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-04-07 23:19:05 -0400

    from scratch done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,25 +1,41 @@
-const addToFrontOrBack = () => {
+const addToFrontOrBack = (arr, value, isFront) => {
+  if(isFront){
+    arr.unshift(value);
+  } else {
+    arr.push(value);
+  }
 };
 
-const reverseString = () => {
+const reverseString = (str) => {
+  return str.split("").reverse().join("")
 };
 
-const newArrayFullOf = () => {
+const newArrayFullOf = (value, numOfValue) => {
+  const newArr = new Array(numOfValue).fill(value);
+  return newArr;
 };
 
-const insertIntoMiddle = () => {
+const insertIntoMiddle = (arr,value) => {
+  const mid = Math.floor(arr.length/2)
+  arr.splice(mid,0,value);
 };
 
-const deleteFromMiddle = () => {
+const deleteFromMiddle = (arr) => {
+  const mid = Math.floor(arr.length/2)
+  arr.splice(mid,1);
 };
 
-const isRightIndex = () => {
+const isRightIndex = (arr,value,index) => {
+  return arr[index] === value 
 };
 
-const roundAllNumsDown = () => {
+const roundAllNumsDown = (arr) => {
+  const copy = arr.map(val => Math.floor(val))
+  return copy;
 };
 
-const getAllYCoordinates = () => {
+const getAllYCoordinates = (arrOfCoords) => {
+  return arrOfCoords.map(arr => arr[1])
 };
 
 module.exports = {
